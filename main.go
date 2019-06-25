0104223492ec7ea02a3fb508c9d791b4a98e28b8
Xiangrui2019 <xiangrui@aiursoft.com>
2019-06-26 06:31:53 +0800

    Add server to http.Server{} struct

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/xiangrui2019/go-kahla-bot-server/conf"
 	"github.com/xiangrui2019/go-kahla-bot-server/injects"
 	"github.com/xiangrui2019/go-kahla-bot-server/routers"
 	"github.com/xiangrui2019/go-kahla-bot-server/server"
 	"gopkg.in/macaron.v1"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -20,25 +22,25 @@ func main() {
 	interrupt2 := make(chan struct{})
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = config.ConfigEnvironment()
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = router.ConfigureRouting()
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = injector.Inject()
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	go func() {
@@ -54,5 +56,16 @@ func main() {
 		}
 	}()
 
-	app.Run(config.Host, config.Port)
+	httpserver := http.Server{
+		Addr: fmt.Sprintf("%s:%d", config.Host, config.Port),
+		Handler: app,
+	}
+
+	log.Printf("listening on %s (%s)\n", fmt.Sprintf("%s:%d", config.Host, config.Port), macaron.Env)
+
+	err = httpserver.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
