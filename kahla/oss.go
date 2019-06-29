335223d10aaf5ad29e2f9c42e47adcad3052eac2
Xiangrui2019 <xiangrui@aiursoft.com>
2019-06-29 10:42:27 +0800

    change oss key to uint32

--- kahla/oss.go
+++ kahla/oss.go
@@ -16,11 +16,11 @@ func NewOssService(client *http.Client, baseUrl string) *OssService {
 	return &OssService{client: client, baseUrl: baseUrl}
 }
 
-func (s *OssService) HeadImgFile(headImgFileKey int, w int, h int) ([]byte, *http.Response, error) {
+func (s *OssService) HeadImgFile(headImgFileKey uint32, w int, h int) ([]byte, *http.Response, error) {
 	v := url.Values{}
 	v.Set("w", strconv.Itoa(w))
 	v.Set("h", strconv.Itoa(h))
-	resp, err := s.client.Get("https://oss.aiursoft.com/Download/FromKey/" + strconv.Itoa(headImgFileKey) + "?" + v.Encode())
+	resp, err := s.client.Get("https://oss.aiursoft.com/Download/FromKey/%d" + strconv.FormatUint(uint64(headImgFileKey), 10) + "?" + v.Encode())
 	if err != nil {
 		return nil, resp, err
 	}
